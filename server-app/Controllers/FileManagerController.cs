13f407f5ece111218d320b7f86205bf23197b8dc
Bharat Ram <bharatram.h@syncfusion.com>
2024-07-01 12:01:57 +0530

    891562: Need to move the latest changes to public repo

---
+++ server-app/Controllers/FileManagerController.cs
@@ -0,0 +1,326 @@
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+//File Manager's base functions are available in the below namespace
+using DocumentExplorer.Models.FileManager;
+//File Manager's operations are available in the below namespace
+using DocumentExplorer.Data;
+using Newtonsoft.Json;
+using System.Linq;
+using System.IO;
+using Newtonsoft.Json.Serialization;
+using Microsoft.AspNetCore.Cors;
+
+namespace DocumentExplorer.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class FileManagerController : ControllerBase
+    {
+        private PhysicalFileProvider operation;
+        private string basePath;
+        string root = "wwwroot\\Files";
+        string rootPath;
+        string virtualConnection;
+        public FileManagerController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.operation = new PhysicalFileProvider();
+            if (basePath.EndsWith("\\"))
+                this.rootPath = this.basePath + this.root;
+            else
+                this.rootPath = this.basePath + "\\" + this.root;
+            this.operation.RootFolder(rootPath);
+        }
+        // Processing the File Manager operations
+        [Route("FileOperations")]
+        public object FileOperations([FromBody] FileManagerCustomContent args)
+        {
+
+            args.RootType = HttpContext.Request.Headers["RootType"];
+            if (basePath.EndsWith("\\"))
+            {
+                virtualConnection = this.basePath + "wwwroot\\VirtualConnections";
+            }
+            else
+            {
+                virtualConnection = this.basePath + "\\wwwroot\\VirtualConnections";
+            }
+            DateTime currentTime = DateTime.Now;
+            DateTime deletionThreshold = currentTime.AddHours(-24);
+            DirectoryInfo virtualDirectoryInfo = new DirectoryInfo(virtualConnection);
+
+            if (Directory.Exists(virtualConnection) && virtualDirectoryInfo.CreationTime <= deletionThreshold)
+            {
+                Directory.Delete(virtualConnection, true);
+            }
+            if (!Directory.Exists(virtualConnection))
+            {
+                //Create virtual  root directory
+                Directory.CreateDirectory(virtualConnection);
+            }
+            string userID = virtualConnection + "\\" + HttpContext.Connection.Id + "\\Files";
+            string virtualUser = virtualConnection + "\\" + HttpContext.Connection.Id + "\\User";
+            string virtualTrash = virtualConnection + "\\" + HttpContext.Connection.Id + "\\Trash";
+            if (!Directory.Exists(userID))
+            {
+                Directory.CreateDirectory(userID);
+                Directory.CreateDirectory(virtualUser);
+                Directory.CreateDirectory(virtualTrash);
+                CopyFolder(rootPath, userID);
+                CopyFolder(this.basePath + "\\wwwroot\\User", virtualUser);
+                CopyFolder(this.basePath + "\\wwwroot\\Trash", virtualTrash);
+            }
+
+            //Set user directory as root
+            this.operation.RootFolder(userID);
+
+            if (args.Action == "delete" || args.Action == "rename")
+            {
+                if ((args.TargetPath == null) && (args.Path == ""))
+                {
+                    FileManagerResponse response = new FileManagerResponse();
+                    response.Error = new ErrorDetails { Code = "401", Message = "Restricted to modify the root folder." };
+                    return this.operation.ToCamelCase(response);
+                }
+            }
+
+            switch (args.Action)
+            {
+                // Add your custom action here
+                case "read":
+                    if ((args.RootType != null) && ((args.RootType == "Recent")))
+                    {
+                        FileManagerResponse result1 = this.operation.Search(args.Path, "*", args.ShowHiddenItems, false);
+                        return FilterRecentFiles(result1);
+                    }
+                    else
+                    {
+                        return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                    }
+                case "delete":
+
+#if Publish
+                    FileManagerResponse deleteResponse = new FileManagerResponse();
+                    deleteResponse.Error = new ErrorDetails() { Code = "401", Message = "Restricted to perform this action" };
+                    return this.operation.ToCamelCase(deleteResponse);
+#else
+                    FileManagerDirectoryContent[] items1 = args.Data;
+                    string[] names1 = args.Names;
+                    return this.operation.ToCamelCase(MoveToTrash(args.Data, HttpContext.Connection.Id, virtualConnection, virtualUser));
+#endif
+                case "copy":
+                case "move":
+                    FileManagerResponse response = new FileManagerResponse();
+                    response.Error = new ErrorDetails() { Code = "401", Message = "Restricted to perform this action" };
+                    return this.operation.ToCamelCase(response);
+                case "details":
+                    if ((args.RootType != null) && ((args.RootType == "Recent")))
+                    {
+                        FileManagerDirectoryContent[] items = args.Data;
+                        string[] names = args.Names;
+                        for (var i = 0; i < items.Length; i++)
+                        {
+                            names[i] = ((items[i].FilterPath + items[i].Name).Substring(1));
+                        }
+                        return this.operation.ToCamelCase(this.operation.Details("/", names, args.Data));
+                    }
+                    else
+                    {
+                        return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names, args.Data));
+                    }
+                case "create":
+                    // Path - Current path where the folder is to be created; Name - Name of the new folder
+                    return this.operation.ToCamelCase(this.operation.Create(args.Path, args.Name));
+                case "search":
+                    // Path - Current path where the search is performed; SearchString - String typed in the searchbox; CaseSensitive - Boolean value which specifies whether the search must be casesensitive                    
+                    if ((args.RootType != null) && ((args.RootType == "Recent")))
+                    {
+                        FileManagerResponse result1 = this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive);
+                        return FilterRecentFiles(result1);
+                    }
+                    else
+                    {
+                        return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                    }
+                case "rename":
+                    // Path - Current path of the renamed file; Name - Old file name; NewName - New file name
+                    if ((args.RootType != null) && (args.RootType == "Recent"))
+                    {
+                        var items = args.Data;
+                        var name = ((items[0].FilterPath + items[0].Name).Substring(1));
+                        var newName = ((items[0].FilterPath + args.NewName).Substring(1));
+                        return this.operation.ToCamelCase(this.operation.Rename("/", name, newName));
+                    }
+                    else
+                    {
+                        return this.operation.ToCamelCase(this.operation.Rename(args.Path, args.Name, args.NewName));
+                    }
+            }
+            return null;
+        }
+        public FileManagerResponse FilterRecentFiles(FileManagerResponse result)
+        {
+            IEnumerable<FileManagerDirectoryContent> allFiles = (result.Files);
+            allFiles = allFiles?.Where(item => item.DateModified.AddDays(5).CompareTo(DateTime.Now) != -1 && item.IsFile == true);
+            result.Files = allFiles;
+            return result;
+        }
+        public FileManagerResponse MoveToTrash(FileManagerDirectoryContent[] dataArray, string userId, String virtualConnection, string virtualUser)
+        {
+            string jsonPath = virtualUser + "\\trash.json";
+            string jsonData = System.IO.File.ReadAllText(jsonPath);
+            List<TrashContents> DeletedFiles = JsonConvert.DeserializeObject<List<TrashContents>>(jsonData) ?? new List<TrashContents>();
+            PhysicalFileProvider trashOperation = new PhysicalFileProvider();
+            string root = virtualConnection + "\\" + userId;
+            trashOperation.RootFolder(root);
+            List<FileManagerDirectoryContent> deletedFiles = new List<FileManagerDirectoryContent>();
+            foreach (FileManagerDirectoryContent data in dataArray)
+            {
+                string fileLocation = "/Files" + data.FilterPath;
+                DateTime deleteTime = DateTime.Now;
+                string container = deleteTime.ToFileTimeUtc().ToString();
+                string trashPath = "/Trash/" + container;
+                Directory.CreateDirectory(root + trashPath);
+                FileManagerResponse response = trashOperation.Move(fileLocation, trashPath, new string[] { data.Name }, null, null, null);
+                if ((response.Error == null))
+                {
+                    TrashContents deletedFile = new TrashContents()
+                    {
+                        Container = container,
+                        Data = data,
+                        DateDeleted = deleteTime,
+                        Name = data.Name,
+                        Path = data.FilterPath
+                    };
+                    deletedFile.Data.DateModified = deletedFile.DateDeleted;
+                    deletedFile.Data.Id = deletedFile.Container;
+                    DeletedFiles.Add(deletedFile);
+                    deletedFiles.Add(response.Files.First());
+                }
+            }
+            jsonData = JsonConvert.SerializeObject(DeletedFiles);
+            System.IO.File.WriteAllText(jsonPath, jsonData);
+            return new FileManagerResponse() { Files = deletedFiles };
+        }
+        [Route("Upload")]
+        public IActionResult Upload(string path, IList<IFormFile> uploadFiles, string action)
+        {
+#if Publish
+            //Restrict the upload functionality for publish settings
+            Response.Clear();
+            Response.ContentType = "application/json; charset=utf-8";
+            Response.StatusCode = 403;
+            Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = "The upload functionality is restricted in this online demo. To test this demo application with upload functionality, please download the source code from the GitHub location (https://github.com/syncfusion/blazor-showcase-document-explorer) and run it.";
+            return Content("The upload functionality is restricted in this online demo. To test this demo application with upload functionality, please download the source code from the GitHub location (https://github.com/syncfusion/blazor-showcase-document-explorer) and run it.");
+#else
+            FileManagerResponse uploadResponse;
+            PhysicalFileProvider uploadOperation = new PhysicalFileProvider();
+            string basePath = this.basePath + "\\wwwroot\\VirtualConnections\\" + HttpContext.Connection.Id;
+            string userID = this.basePath + "wwwroot\\VirtualConnections\\" + HttpContext.Connection.Id + "\\Files";
+            uploadOperation.RootFolder(userID);
+            uploadResponse = operation.Upload(path, uploadFiles, action, basePath, null);
+            if (uploadResponse.Error != null)
+            {
+                Response.Clear();
+                Response.ContentType = "application/json; charset=utf-8";
+                Response.StatusCode = Convert.ToInt32(uploadResponse.Error.Code);
+                Response.HttpContext.Features.Get<IHttpResponseFeature>().ReasonPhrase = uploadResponse.Error.Message;
+            }
+            return Content("");
+#endif
+        }
+
+        [Route("Download")]
+        public IActionResult Download(string downloadInput)
+        {
+            FileManagerDirectoryContent args = JsonConvert.DeserializeObject<FileManagerDirectoryContent>(downloadInput);
+            FileManagerDirectoryContent[] items = args.Data;
+            string[] names = args.Names;
+            for (var i = 0; i < items.Length; i++)
+            {
+                names[i] = ((items[i].FilterPath + items[i].Name).Substring(1));
+            }
+            return operation.Download("/", names);
+        }
+
+        [Route("ToggleStarred")]
+        public IActionResult ToggleStarred([FromBody] FileManagerCustomContent args)
+        {
+            string basePath = this.basePath + "\\wwwroot\\VirtualConnections\\" + HttpContext.Connection.Id +"\\User";
+            string jsonPath = basePath + "\\star.json";
+            StreamReader reader = new StreamReader(jsonPath);
+            string jsonData = reader.ReadToEnd();
+            reader.Dispose();
+            List<string> starredFiles = JsonConvert.DeserializeObject<List<string>>(jsonData) ?? new List<string>();
+            string path = args.Path.Replace(Path.DirectorySeparatorChar, '/');
+            if (args.Starred && !starredFiles.Contains(path))
+            {
+                starredFiles.Add(path);
+            }
+            else if (!args.Starred && starredFiles.Contains(path))
+            {
+                starredFiles.Remove(path);
+            }
+            jsonData = JsonConvert.SerializeObject(starredFiles);
+            System.IO.File.WriteAllText(jsonPath, jsonData);
+            return Content("");
+        }
+
+        [Route("GetImage")]
+        public IActionResult GetImage(FileManagerDirectoryContent args)
+        {
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+
+        private void CopyFolder(string source, string destination)
+        {
+            if (!Directory.Exists(destination))
+            {
+                Directory.CreateDirectory(destination);
+            }
+
+            foreach (var file in Directory.EnumerateFiles(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(file));
+                System.IO.File.Copy(file, dest);
+            }
+
+            foreach (var folder in Directory.EnumerateDirectories(source))
+            {
+                var dest = Path.Combine(destination, Path.GetFileName(folder));
+                CopyFolder(folder, dest);
+            }
+        }
+
+        public class FileManagerCustomContent : FileManagerDirectoryContent
+        {
+            public string RootType { get; set; }
+
+            //Custom parameter inidicating starred files
+            public bool Starred { get; set; }
+        }
+        public class FileResponse
+        {
+            public FileManagerDirectoryContent CWD { get; set; }
+
+            public IEnumerable<FileManagerCustomContent> Files { get; set; }
+
+            public ErrorDetails Error { get; set; }
+
+            public FileDetails Details { get; set; }
+
+        }
+        public class TrashContents
+        {
+            public string Container { get; set; }
+            public DateTime DateDeleted { get; set; }
+            public string Path { get; set; }
+            public string Name { get; set; }
+            public FileManagerDirectoryContent Data { get; set; }
+        }
+    }
+}
---
+++ webapp/server/DocumentExplorer/Controllers/SharedFilesController.cs
@@ -0,0 +1,199 @@
+﻿using DocumentExplorer.Shared;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http.Features;
+//File Manager's base functions are available in the below namespace
+using DocumentExplorer.Models.FileManager;
+//File Manager's operations are available in the below namespace
+using DocumentExplorer.Data;
+using Newtonsoft.Json;
+using System.Linq;
+using System.Threading.Tasks;
+using System.IO;
+using Syncfusion.DocIO.DLS;
+using Syncfusion.DocIORenderer;
+using Syncfusion.Blazor.PdfViewer;
+using Syncfusion.Pdf;
+using Syncfusion.Presentation;
+using Syncfusion.PresentationRenderer;
+using System.Drawing;
+using Microsoft.AspNetCore.Cors;
+using DocumentExplorer.Models;
+using SkiaSharp;
+
+namespace DocumentExplorer.Controllers
+{
+    [Route("api/[controller]")]
+    [EnableCors("AllowAllOrigins")]
+    public class SharedFilesController : ControllerBase
+    {
+        private PhysicalFileProvider operation;
+        private string basePath;
+        public SharedFilesController(IWebHostEnvironment hostingEnvironment)
+        {
+            this.basePath = hostingEnvironment.ContentRootPath;
+            this.operation = new PhysicalFileProvider();
+            this.operation.RootFolder(this.basePath + "\\wwwroot\\SharedFiles"); // Data\\Files denotes in which files and folders are available.
+        }
+
+        // Processing the File Manager operations
+        [Route("FileOperations")]
+        public object FileOperations([FromBody] FileManagerFilterContent args)
+        {
+            if (args.Path == "/Files/")
+            {
+                args.Path = "/";
+            }
+            switch (args.Action)
+            {
+                // Add your custom action here
+                case "read":
+                    // Path - Current path; ShowHiddenItems - Boolean value to show/hide hidden items
+                    return this.operation.ToCamelCase(this.operation.GetFiles(args.Path, args.ShowHiddenItems));
+                case "details":
+                    // Path - Current path where details of file/folder is requested; Name - Names of the requested folders
+                    return this.operation.ToCamelCase(this.operation.Details(args.Path, args.Names));
+                case "create":
+                    FileManagerResponse createresponse = new FileManagerResponse();
+                    createresponse.Error = new ErrorDetails() { Code = "401", Message = "Restricted to perform this action" };
+                    return this.operation.ToCamelCase(createresponse);
+                case "search":
+                    // Path - Current path where the search is performed; SearchString - String typed in the searchbox; CaseSensitive - Boolean value which specifies whether the search must be casesensitive
+                    return this.operation.ToCamelCase(this.operation.Search(args.Path, args.SearchString, args.ShowHiddenItems, args.CaseSensitive));
+                case "delete":
+                case "copy":
+                case "move":
+                case "rename":
+                    FileManagerResponse renameresponse = new FileManagerResponse();
+                    renameresponse.Error = new ErrorDetails() { Code = "401", Message = "Restricted to perform this action" };
+                    return this.operation.ToCamelCase(renameresponse);
+            }
+            return null;
+        }
+
+        [Route("Download")]
+        public IActionResult Download(string downloadInput)
+        {
+
+            FileManagerFilterContent args = JsonConvert.DeserializeObject<FileManagerFilterContent>(downloadInput);
+            FileManagerDirectoryContent[] items = args.Data;
+            string[] names = args.Names;
+            for (var i = 0; i < items.Length; i++)
+            {
+                names[i] = ((items[i].FilterPath + items[i].Name).Substring(1));
+            }
+            return operation.Download("/", names);
+        }
+
+
+        [Route("GetImage")]
+        public IActionResult GetImage(FileManagerFilterContent args)
+        {
+            return this.operation.GetImage(args.Path, args.Id, false, null, null);
+        }
+        [Route("GetPreviewImage")]
+        public IActionResult GetPreviewImage(FileManagerFilterContent args)
+        {
+            string baseFolder = this.basePath + "\\wwwroot\\SharedFiles";
+
+            try
+            {
+                String fullPath = baseFolder + args.Path;
+                string extension = Path.GetExtension(fullPath);
+                Stream imageStream = null;
+                if (extension == Constants.Pdf)
+                {
+                    FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                    PdfRenderer pdfExportImage = new PdfRenderer();
+                    //Loads the PDF document 
+                    pdfExportImage.Load(fileStream);
+                    //Exports the PDF document pages into images
+                    SkiaSharp.SKBitmap[] skBitmaps = pdfExportImage.ExportAsImage(0, 0);
+                    System.Drawing.Bitmap[] bitmapImages = new System.Drawing.Bitmap[skBitmaps.Length];
+
+                    for (int i = 0; i < skBitmaps.Length; i++)
+                    {
+                        using (SKImage skImage = SKImage.FromBitmap(skBitmaps[i]))
+                        using (SKData skData = skImage.Encode(SKEncodedImageFormat.Png, 100))
+                        using (System.IO.MemoryStream stream = new System.IO.MemoryStream(skData.ToArray()))
+                        {
+                            bitmapImages[i] = new System.Drawing.Bitmap(stream);
+                        }
+                    }
+                    imageStream = new MemoryStream();
+                    bitmapImages[0].Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
+                    imageStream.Position = 0;
+                    pdfExportImage.Dispose();
+                    fileStream.Close();
+                }
+                else if (extension == Constants.Docx || extension == Constants.Rtf || extension == Constants.Doc)
+                {
+                    FileStream fileStream = new FileStream(fullPath, FileMode.Open, FileAccess.Read);
+                    //Loads file stream into Word document
+                    WordDocument document = new WordDocument(fileStream, Syncfusion.DocIO.FormatType.Automatic);
+                    fileStream.Dispose();
+                    //Instantiation of DocIORenderer for Word to PDF conversion
+                    DocIORenderer render = new DocIORenderer();
+                    //Converts Word document into PDF document
+                    PdfDocument pdfDocument = render.ConvertToPDF(document);
+                    //Releases all resources used by the Word document and DocIO Renderer objects
+                    render.Dispose();
+                    document.Dispose();
+                    //Saves the PDF file
+                    MemoryStream outputStream = new MemoryStream();
+                    pdfDocument.Save(outputStream);
+                    outputStream.Position = 0;
+                    //Closes the instance of PDF document object
+                    pdfDocument.Close();
+
+                    PdfRenderer pdfExportImage = new PdfRenderer();
+                    //Loads the PDF document 
+                    pdfExportImage.Load(outputStream);
+                    //Exports the PDF document pages into images
+                    SkiaSharp.SKBitmap[] skBitmaps = pdfExportImage.ExportAsImage(0, 0);
+                    System.Drawing.Bitmap[] bitmapImages = new System.Drawing.Bitmap[skBitmaps.Length];
+
+                    for (int i = 0; i < skBitmaps.Length; i++)
+                    {
+                        using (SKImage skImage = SKImage.FromBitmap(skBitmaps[i]))
+                        using (SKData skData = skImage.Encode(SKEncodedImageFormat.Png, 100))
+                        using (System.IO.MemoryStream stream = new System.IO.MemoryStream(skData.ToArray()))
+                        {
+                            bitmapImages[i] = new System.Drawing.Bitmap(stream);
+                        }
+                    }
+                    imageStream = new MemoryStream();
+                    bitmapImages[0].Save(imageStream, System.Drawing.Imaging.ImageFormat.Png);
+                    imageStream.Position = 0;
+
+                    fileStream.Close();
+                }
+                else if (extension == Constants.Pptx)
+                {
+                    IPresentation presentation = Presentation.Open(fullPath);
+                    //Initialize PresentationRenderer for image conversion
+                    presentation.PresentationRenderer = new PresentationRenderer();
+                    //Convert the first slide to image
+                    imageStream = presentation.Slides[0].ConvertToImage(ExportImageFormat.Png);
+                    presentation.Dispose();
+                }
+                FileStreamResult fileStreamResult = new FileStreamResult(imageStream, "APPLICATION/octet-stream");
+                return fileStreamResult;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+
+        public class FileManagerFilterContent : FileManagerDirectoryContent
+        {
+            public string RootType { get; set; }
+        }
+
+    }
+}
