3bd21efb6133321bcc3a03f5a56a09a344f1984f
VikramSaravanan <vikram.saravanan@syncfusion.com>
2026-09-25 20:35:51 +0530

    1054255: updating the SEO changes

--- server/Program.cs
+++ server/Program.cs
@@ -56,6 +56,14 @@ if (!app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// SEO: Add X-Robots-Tag header for search engine indexing
+app.Use(async (context, next) =>
+{
+    context.Response.Headers.Append("X-Robots-Tag", "index, follow");
+    await next();
+});
+
 app.UseSession();
 app.MapStaticAssets();
 app.UseAntiforgery();
